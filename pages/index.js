1cdb6568ab413baafcd61a5f7b18f0dc1fdeb9e6
shimazaki <lozld30@gmail.com>
2021-05-30 12:52:32 +0900

    noindex

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+		<meta name="robots" content="noindex" />
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
